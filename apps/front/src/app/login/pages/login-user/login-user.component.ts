b993b7b94a1555797c9b2eeda311c4fd9b86688a
David Hernandez <davidmiguelh@gmail.com>
2021-02-16 17:16:35 -0600

    Agregamos la estructura propuesta para la plantilla

---
+++ apps/front/src/app/login/pages/login-user/login-user.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'comfeco-login-user',
+  templateUrl: './login-user.component.html',
+  styleUrls: ['./login-user.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class LoginUserComponent implements OnInit {
+
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
