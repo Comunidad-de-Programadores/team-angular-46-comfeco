55e85cb5f4084c07489a73e3dd8181a1d18d52bd
Ruben Salgado Ramirez <ing.ruben.salgado.ramirez@gmail.com>
2021-02-28 22:23:43 -0600

    Creación de contenedor para las vistas de workshops y communities

---
+++ apps/front/src/app/comfeco/@core/utils.component.ts
@@ -0,0 +1,8 @@
+
+export class ComponentConfeco {
+
+  static goToLink(link:string) {
+    window.open(link, "_blank");
+  }
+
+}
---
+++ apps/front/src/app/comfeco/pages/article-container/article-container.component.ts
@@ -0,0 +1,32 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, Output, SimpleChanges, ViewEncapsulation } from '@angular/core';
+import { ComboInterface } from '@comfeco/interfaces';
+
+@Component({
+  selector: 'comfeco-article-container',
+  templateUrl: './article-container.component.html',
+  styleUrls: ['./article-container.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ArticleContainerComponent {
+
+  @Input() title:string;
+  @Input() viewMore:string;
+  @Input() comboBox:boolean = false;
+  @Input() comboData:ComboInterface[] = [];
+  @Input() comboTitleDefault:string;
+  @Output() onComboOptionSelected: EventEmitter<string> = new EventEmitter();
+  
+  showWorkshop:boolean;
+
+  constructor() { }
+
+  toggleWorkshop() {
+    this.showWorkshop = !this.showWorkshop;
+  }
+
+  onOptionSelected(evento) {
+    this.onComboOptionSelected.emit( evento );
+  }
+
+}
