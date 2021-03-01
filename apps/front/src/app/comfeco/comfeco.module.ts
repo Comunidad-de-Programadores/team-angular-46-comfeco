4a20b141b75c9d090ca3fa5aa4e363edbd93df17
Ruben Salgado Ramirez <ing.ruben.salgado.ramirez@gmail.com>
2021-02-28 22:55:12 -0600

    Creación del componente del dashboard

--- apps/front/src/app/comfeco/comfeco.module.ts
+++ apps/front/src/app/comfeco/comfeco.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { ComfecoRoutingModule, Components } from './comfeco-routing.module';
 import { ThemeComfecoModule } from './@theme/theme.module';
+import { DashboardService } from './pages/page-dashboard/page-dashboard.service';
 
 @NgModule({
   declarations: [
     ...Components
+  ],
+  providers: [
+    DashboardService,
   ],
   imports: [
     CommonModule,
--- apps/front/src/app/comfeco/pages/page-dashboard/page-dashboard.component.ts
+++ apps/front/src/app/comfeco/pages/page-dashboard/page-dashboard.component.ts
@@ -1,4 +1,10 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, OnDestroy } from '@angular/core';
+import { Subject } from "rxjs";
+
+import { AreaWorkshopDto, ComboInterface, CommunityDto, DayEvent, EventDto, KnowledgeAreaDto, WorkshopAreaDto } from '@comfeco/interfaces';
+
+import { DashboardService } from './page-dashboard.service';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'comfeco-page-dashboard',
@@ -6,8 +12,108 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./page-dashboard.component.scss'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class PageDashboardComponent {
+export class PageDashboardComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  communities:CommunityDto[];
+  comboKnowledgeArea:ComboInterface[] = [];
+  workshops:WorkshopAreaDto[] = [];
+  idArea:string;
+  eventInfo:EventDto;
+  dayEvent:DayEvent;
+
+  messageErrorCommunities:string;
+  messageErrorWorkshops:string;
+
+  unsubscribe$ = new Subject<void>();
+
+  constructor(private _service: DashboardService) { }
+
+  ngOnInit(): void {
+    this._service.communities()
+      .subscribe(
+        (resp:any) => {
+          if(resp.success) {
+            if(resp.communities) {
+              this.communities = resp.communities;
+            }
+          } else {
+            this.messageErrorCommunities = resp.message;
+          }
+        }
+      );
+
+    this._service.knowledgeArea()
+      .subscribe(
+        (resp:any) => {
+          if(resp.success) {
+            if(resp.areas) {
+              resp.areas.forEach((areaTemp:KnowledgeAreaDto) => {
+                const { area, id } = areaTemp;
+                const combo:ComboInterface = {
+                  value: area,
+                  id
+                };
+                this.comboKnowledgeArea.push(combo);
+              });
+            }
+          }
+        }
+      );
+
+    this._service.eventInfo()
+      .subscribe(
+        (resp:any) => {
+          if(resp.success) {
+            this.eventInfo = {...resp};
+            this._countdown(new Date(resp.dateEvent));
+          }
+        }
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  changeWorkshops(idArea:string) {
+    this._service.workshops(idArea)
+      .subscribe(
+        (resp:any) => {
+          if(resp.success) {
+            if(resp.areas) {
+              const areas:AreaWorkshopDto = resp.areas;
+              this.workshops = areas[0].workshops;
+              this.messageErrorWorkshops = '';
+            }
+          } else {
+            this.messageErrorWorkshops = resp.message;
+          }
+        }
+      );
+  }
+
+  private _countdown(dateEvent:Date): void {
+    this._service.countdownTimer(dateEvent)
+      .pipe(
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe(resp => {
+        const { days, hours, minutes, seconds, timeOut } = resp;
+        
+        this.dayEvent = {
+          days,
+          hours,
+          minutes,
+          seconds,
+          timeOut
+        };
+
+        if(resp.timeOut) {
+          this.unsubscribe$.next();
+          this.unsubscribe$.complete();
+        }
+      });
+  }
   
 }
