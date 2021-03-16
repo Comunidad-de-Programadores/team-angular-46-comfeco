4b12d664cd075ec1a0a90b74b15d50edee84f767
Ruben Salgado Ramirez <ing.ruben.salgado.ramirez@gmail.com>
2021-03-16 17:11:00 -0600

    Servicio para visualización de la información de los grupos

---
+++ apps/back/src/app/inner/groups/groups.service.ts
@@ -0,0 +1,72 @@
+import { GenericResponse, GroupDto, GroupsDto } from '@comfeco/interfaces';
+import { UtilResponse } from '@comfeco/validator';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { GroupsRepository } from './groups.repository';
+
+@Injectable()
+export class GroupsService {
+
+    constructor(private readonly _groupsRepository: GroupsRepository) {}
+
+     
+    async groups(idUser:string): Promise<GroupsDto | GenericResponse> {
+        const groupsEntity:GroupDto[] = await this._groupsRepository.groups(idUser);
+        
+        if(groupsEntity==null) {
+            return UtilResponse.genericResponse('',['No hay grupos activos en la base de datos'], HttpStatus.NOT_FOUND);
+        }
+        
+        return this._filterGroup(groupsEntity);
+    }
+
+    async groupContainsName(idUser:string, name:string) {
+        if(!name) {
+            return UtilResponse.genericResponse('',['Es necesario enviar un nombre para poder realizar la busqueda'], HttpStatus.NOT_FOUND);
+        }
+
+        const nameGroup:string = name.toLowerCase();
+
+        const groupsEntity:GroupDto[] = await this._groupsRepository.groupContainsName(idUser, nameGroup);
+        
+        if(groupsEntity==null) {
+            return UtilResponse.genericResponse('',['No hay coincidencias con ese nombre de grupo'], HttpStatus.NOT_FOUND);
+        }
+        
+        return this._filterGroup(groupsEntity);
+    }
+
+    async groupByLanguage(idUser:string, languageId:string) {
+        if(languageId==='-1') {
+            return this.groups(idUser);
+        }
+        
+        if(!languageId) {
+            return UtilResponse.genericResponse('',['Es necesario enviar el id para mostrar la información del grupo'], HttpStatus.NOT_FOUND);
+        }
+
+        const groupsEntity:GroupDto[] = await this._groupsRepository.groupByLanguage(idUser, languageId);
+        
+        if(groupsEntity==null) {
+            return UtilResponse.genericResponse('',['No hay coincidencias con ese id de grupo'], HttpStatus.NOT_FOUND);
+        }
+        
+        return this._filterGroup(groupsEntity);
+    }
+
+    private _filterGroup(groupsEntity:GroupDto[]) {
+        const groupsInformation:GroupDto[] = [];
+
+        groupsEntity.forEach((group:GroupDto) => {
+            const { id, name:groupName, language, penality, description, belong } = group;
+            groupsInformation.push({ id, name:groupName, language, penality, description, belong });
+        });
+
+        const groups:GroupsDto = {
+            code: HttpStatus.OK,
+            groups: groupsInformation
+        }
+
+        return groups;
+    }
+
+}
