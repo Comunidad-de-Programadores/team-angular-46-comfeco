4b19e993fc8abc94e097bf5963acde829669d64d
Ruben Salgado Ramirez <ing.ruben.salgado.ramirez@gmail.com>
2021-03-16 16:41:59 -0600

    Creación de la tarjeta de grupo. Close #84

---
+++ apps/front/src/app/comfeco/pages/card-group/card-group.component.ts
@@ -0,0 +1,27 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
+
+import { GroupDto } from '@comfeco/interfaces';
+
+@Component({
+  selector: 'comfeco-card-group',
+  templateUrl: './card-group.component.html',
+  styleUrls: ['./card-group.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CardGroupComponent {
+
+  @Input() group:GroupDto;
+  
+  @Output() onJoinGroup: EventEmitter<GroupDto> = new EventEmitter();
+  @Output() onLeaveGroup: EventEmitter<GroupDto> = new EventEmitter();
+
+  onJoin() {
+    this.onJoinGroup.emit(this.group);
+  }
+
+  onLeave() {
+    this.onLeaveGroup.emit(this.group);
+  }
+
+}
